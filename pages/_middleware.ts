9e40a70cd4b5ca1bed528c98a36c4bee778ece08
James Rushford <45968181+Crushford@users.noreply.github.com>
2022-01-06 23:16:22 +0000

    git pathname bug

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest, res: NextResponse) {
-  if (req.url === '/') {
+  const { pathname } = req.nextUrl
+
+  console.log(pathname)
+  if (pathname === '/') {
     const country = req.geo.country || 'de'
-    console.log(req)
     const res = NextResponse.rewrite(`/${country}`)
 
     return res
