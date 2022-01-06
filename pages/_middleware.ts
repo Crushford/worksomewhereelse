675280c8b5b28bac52411a95ce9dc07418b9afc6
James Rushford <45968181+Crushford@users.noreply.github.com>
2022-01-06 23:17:00 +0000

    remove leftovers

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest, res: NextResponse) {
   const { pathname } = req.nextUrl
 
-  console.log(pathname)
   if (pathname === '/') {
     const country = req.geo.country || 'de'
     const res = NextResponse.rewrite(`/${country}`)
